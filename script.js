88041fd53dd48d2b81f29113d995345907e8a629
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-02-21 20:02:02 -0500

    again new images link

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function updateTimerWrapper(time) {
 function scatterDeck(deck) {
   deck.forEach((card) => {
     gameArea.innerHTML += `
-      <div class="card ${card.suit}-${card.rank}" style="background-image: url(https://nathan-boquiren.github.io/52-pickup-card-game/card-images/${card.suit}-${card.rank}.png)">
+      <div class="card ${card.suit}-${card.rank}" style="background-image: url(/card-images/${card.suit}-${card.rank}.png)">
       </div>`;
     scatterCard(`${card.suit}-${card.rank}`);
   });
